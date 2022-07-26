901d63d694b6717bf47da9a2597b205a42439b7f
SBauer <Stepan.Bauer@retailbusinessservice.com>
2022-07-26 22:33:00 +0200

    fix property for mongo

--- src/main/kotlin/com/example/halooglasi_telegram_bot/controller/InitialLoadController.kt
+++ src/main/kotlin/com/example/halooglasi_telegram_bot/controller/InitialLoadController.kt
@@ -1,6 +1,8 @@
 package com.example.halooglasi_telegram_bot.controller
 
 import com.example.halooglasi_telegram_bot.loader.ApartmentLoader
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("init")
 class InitialLoadController(val apartmentLoader: ApartmentLoader) {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @GetMapping()
-    fun init() {
+    fun init() : ResponseEntity<Unit> {
+        logger.warn("base initialization have been started")
         apartmentLoader.loadWithoutNotify()
+        logger.warn("base initialization have been finished")
+
+        return ResponseEntity.ok().build()
     }
 }
