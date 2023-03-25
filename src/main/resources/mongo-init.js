36f5bd62373eaa44e927ed008e7e6ecf2b077e06
SBauer <Stepan.Bauer@retailbusinessservice.com>
2023-03-25 18:27:41 +0100

    add docker-compose.yml and faq

---
+++ src/main/resources/mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+        {
+            user: "step4ik",
+            pwd: "11123654Aa",
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "apartments"
+                }
+            ]
+        }
+);
