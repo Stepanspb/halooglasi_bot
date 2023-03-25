1b5db959fed9de611ec66f63540a95dbd3107dc9
SBauer <Stepan.Bauer@retailbusinessservice.com>
2023-03-25 18:49:21 +0100

    add sleep time into telegram sender

--- src/main/kotlin/com/example/halooglasi_telegram_bot/loader/ApartmentLoader.kt
+++ src/main/kotlin/com/example/halooglasi_telegram_bot/loader/ApartmentLoader.kt
@@ -41,7 +41,7 @@ class ApartmentLoader(
                     .substringBefore(";var ")
                 val apartments = objectMapper.readValue(json, ApartmentResponse::class.java)
                 processApartments(apartments, notifyEnabled)
-                Thread.sleep(10000)
+                Thread.sleep(5000)
             }
         } catch (e: Exception) {
             logger.error(e.message, e)
@@ -64,6 +64,8 @@ class ApartmentLoader(
                     notifier.notify(newApartment)
                 }
             }
+
+            Thread.sleep(5000)
         }
     }
 }
