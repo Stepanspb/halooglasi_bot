d3b83ab73d3e4d80b87d4baacc7f36fd4b45e317
SBauer <Stepan.Bauer@retailbusinessservice.com>
2023-03-25 12:37:28 +0100

    wait 5 seconds between attemptions

--- src/test/kotlin/com/example/halooglasi_telegram_bot/BaseTest.kt
+++ src/test/kotlin/com/example/halooglasi_telegram_bot/BaseTest.kt
@@ -1,19 +1,10 @@
 package com.example.halooglasi_telegram_bot
 
 import com.example.halooglasi_telegram_bot.dao.ApartmentRepository
-import com.example.halooglasi_telegram_bot.notifier.Notifier
-import com.github.tomakehurst.wiremock.WireMockServer
-import com.github.tomakehurst.wiremock.junit.WireMockRule
-import io.mockk.impl.annotations.MockK
-import org.junit.Rule
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
@@ -21,7 +12,6 @@ import org.springframework.test.context.TestPropertySource
 import org.testcontainers.containers.MongoDBContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
